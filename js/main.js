2594c45210a793bc19fb6a41e43dbb689ce1f5d7
daichungwang-wq <daichungwang@gmail.com>
2026-03-19 15:51:31 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { renderModule1News } from "./modules/module1_news.js?v=5";
-import { renderModule2Health } from "./modules/module2_health.js?v=5";
+import { renderModule2Health } from "./modules/module2_health.js?v=6";
 import { renderModule3Decision } from "./modules/module3_decision.js?v=5";
 
 async function loadJson(path) {
